d51d5b8293a2f4725bb7e45100e99bd8bf21f951
Anton <jaxjaxjaximus@gmail.com>
2021-09-28 13:41:56 +0500

    First

---
+++ ComponentTester-1.44m/dep/ST7565R.o.d
@@ -0,0 +1,111 @@
+ST7565R.o: ST7565R.c config.h config_328.h common.h \
+ /usr/lib/avr/include/stdio.h /usr/lib/avr/include/inttypes.h \
+ /usr/lib/gcc/avr/5.4.0/include/stdint.h /usr/lib/avr/include/stdint.h \
+ /usr/lib/gcc/avr/5.4.0/include/stdarg.h \
+ /usr/lib/gcc/avr/5.4.0/include/stddef.h /usr/lib/avr/include/stdlib.h \
+ /usr/lib/avr/include/string.h /usr/lib/avr/include/math.h \
+ /usr/lib/avr/include/avr/io.h /usr/lib/avr/include/avr/sfr_defs.h \
+ /usr/lib/avr/include/avr/iom328.h /usr/lib/avr/include/avr/iom328p.h \
+ /usr/lib/avr/include/avr/portpins.h /usr/lib/avr/include/avr/common.h \
+ /usr/lib/avr/include/avr/version.h /usr/lib/avr/include/avr/fuse.h \
+ /usr/lib/avr/include/avr/lock.h /usr/lib/avr/include/util/delay.h \
+ /usr/lib/avr/include/util/delay_basic.h /usr/lib/avr/include/avr/sleep.h \
+ /usr/lib/avr/include/avr/eeprom.h /usr/lib/avr/include/avr/pgmspace.h \
+ /usr/lib/avr/include/avr/wdt.h /usr/lib/avr/include/avr/interrupt.h \
+ variables.h colors.h functions.h ST7565R.h bitmaps/font_6x8_vf.h \
+ bitmaps/font_8x8_vf.h bitmaps/font_8x16_vfp.h \
+ bitmaps/font_6x8_iso8859-2_vf.h bitmaps/font_8x8_iso8859-2_vf.h \
+ bitmaps/font_8x12t_iso8859-2_vfp.h bitmaps/font_8x16_iso8859-2_vfp.h \
+ bitmaps/font_8x8_win1251_vf.h bitmaps/font_8x8alt_win1251_vf.h \
+ bitmaps/font_8x8t_win1251_vf.h bitmaps/font_8x12t_win1251_vfp.h \
+ bitmaps/font_8x16_win1251_vfp.h bitmaps/symbols_24x24_vfp.h
+
+config.h:
+
+config_328.h:
+
+common.h:
+
+/usr/lib/avr/include/stdio.h:
+
+/usr/lib/avr/include/inttypes.h:
+
+/usr/lib/gcc/avr/5.4.0/include/stdint.h:
+
+/usr/lib/avr/include/stdint.h:
+
+/usr/lib/gcc/avr/5.4.0/include/stdarg.h:
+
+/usr/lib/gcc/avr/5.4.0/include/stddef.h:
+
+/usr/lib/avr/include/stdlib.h:
+
+/usr/lib/avr/include/string.h:
+
+/usr/lib/avr/include/math.h:
+
+/usr/lib/avr/include/avr/io.h:
+
+/usr/lib/avr/include/avr/sfr_defs.h:
+
+/usr/lib/avr/include/avr/iom328.h:
+
+/usr/lib/avr/include/avr/iom328p.h:
+
+/usr/lib/avr/include/avr/portpins.h:
+
+/usr/lib/avr/include/avr/common.h:
+
+/usr/lib/avr/include/avr/version.h:
+
+/usr/lib/avr/include/avr/fuse.h:
+
+/usr/lib/avr/include/avr/lock.h:
+
+/usr/lib/avr/include/util/delay.h:
+
+/usr/lib/avr/include/util/delay_basic.h:
+
+/usr/lib/avr/include/avr/sleep.h:
+
+/usr/lib/avr/include/avr/eeprom.h:
+
+/usr/lib/avr/include/avr/pgmspace.h:
+
+/usr/lib/avr/include/avr/wdt.h:
+
+/usr/lib/avr/include/avr/interrupt.h:
+
+variables.h:
+
+colors.h:
+
+functions.h:
+
+ST7565R.h:
+
+bitmaps/font_6x8_vf.h:
+
+bitmaps/font_8x8_vf.h:
+
+bitmaps/font_8x16_vfp.h:
+
+bitmaps/font_6x8_iso8859-2_vf.h:
+
+bitmaps/font_8x8_iso8859-2_vf.h:
+
+bitmaps/font_8x12t_iso8859-2_vfp.h:
+
+bitmaps/font_8x16_iso8859-2_vfp.h:
+
+bitmaps/font_8x8_win1251_vf.h:
+
+bitmaps/font_8x8alt_win1251_vf.h:
+
+bitmaps/font_8x8t_win1251_vf.h:
+
+bitmaps/font_8x12t_win1251_vfp.h:
+
+bitmaps/font_8x16_win1251_vfp.h:
+
+bitmaps/symbols_24x24_vfp.h:
